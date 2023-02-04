48e525e6285bbb231fbaf32cf1eede7f2c88f1be
Ravi <ravitomerwmpp2022@gmail.com>
2023-02-04 14:00:40 +0530

    new Change

---
+++ index.js
@@ -0,0 +1,53 @@
+window.onload = function(){
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = 
+document.getElementById('tens')
+    var appendSeconds = 
+document.getElementById('seconds')
+    var buttonStart = 
+document.getElementById('button-Start')
+    var buttonStop = 
+document.getElementById('button-Stop')
+    var buttonReset = 
+document.getElementById('button-Reset')
+    var Interval;
+
+    buttonStart.onclick = function(){
+        clearInterval(Interval);
+        Interval = setInterval(startTimer,10);
+    }
+    buttonStop.onclick = function(){
+        clearInterval(Interval);
+    }
+    buttonReset.onclick = function(){
+    clearInterval(Interval);
+    tens='00';
+    seconds='00';
+    appendTens.innerHTML=tens;
+    appendSeconds.innerHTML=seconds;
+
+}
+    function startTimer(){
+    tens++;
+    if (tens<=9){
+        appendTens.innerHTML='0'+tens;
+
+    }
+    if (tens >9){
+        appendTens.innerHTML = tens;
+    }
+    if (tens>99)
+    {
+        console.log('seconds')
+        seconds++;
+        appendSeconds.innerHTML='0'+seconds;
+        tens=0;
+        appendTens.innerHTML='0'+0;
+    }
+    if(tens>9){
+        appendSeconds.innerHTML=seconds
+    }
+
+}
+}
